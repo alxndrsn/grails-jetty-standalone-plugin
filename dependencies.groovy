65c0662089686ec12c6b299f9253f88ceda72ae3
Alex Anderson <alex@frontlinesms.com>
2011-07-25 18:16:42 +0300

    Updated plugin details.

--- dependencies.groovy
+++ dependencies.groovy
@@ -18,26 +18,18 @@ grails.project.dependency.resolution = {
     }
     dependencies {
         //we only want to do this in the standalone enviorement
-        if (Environment.current.name == "standalone") {
-
-            def jettyVersion = "7.3.1.v20110307"
-            runtime(
-                    "org.eclipse.jetty:jetty-http:${jettyVersion}",
-                    "org.eclipse.jetty:jetty-server:${jettyVersion}",
-                    "org.eclipse.jetty:jetty-webapp:${jettyVersion}",
-                    "org.eclipse.jetty:jetty-plus:${jettyVersion}",
-                    "org.eclipse.jetty:jetty-continuation:${jettyVersion}",
-                    "org.eclipse.jetty:jetty-websocket:${jettyVersion}"
-
-            ) {
-                excludes 'commons-el', 'ant', 'sl4j-api', 'sl4j-simple', 'jcl104-over-slf4j', 'xmlParserAPIs'
-                excludes 'mail', 'commons-lang'
-            }
-	    runtime(
- 		"org.codehaus.groovy.modules.http-builder:http-builder:0.5.0")
-		 {
+        def jettyVersion = "7.3.1.v20110307"
+        build("org.eclipse.jetty:jetty-http:${jettyVersion}",
+                "org.eclipse.jetty:jetty-server:${jettyVersion}",
+                "org.eclipse.jetty:jetty-webapp:${jettyVersion}",
+                "org.eclipse.jetty:jetty-plus:${jettyVersion}",
+                "org.eclipse.jetty:jetty-continuation:${jettyVersion}",
+                "org.eclipse.jetty:jetty-websocket:${jettyVersion}") {
+                    excludes 'commons-el', 'ant', 'sl4j-api', 'sl4j-simple', 'jcl104-over-slf4j', 'xmlParserAPIs'
+                    excludes 'mail', 'commons-lang'
+                }
+        build("org.codehaus.groovy.modules.http-builder:http-builder:0.5.0") {
             		excludes "commons-logging", "xml-apis", "groovy"
-        	}
-    	    }
-	}
+        }
+    }
 }
